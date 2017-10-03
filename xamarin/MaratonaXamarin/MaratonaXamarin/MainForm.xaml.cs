b72030018f07fba5fd345a7b2eb2fe65d56add15
Marcos Farias <marcosfarias@MacBook-Pro-de-Marcos.local>
2017-10-02 21:30:26 -0300

    maratona xamarin

---
+++ xamarin/MaratonaXamarin/MaratonaXamarin/MainForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MaratonaXamarin
+{
+    public partial class MainForm : ContentPage
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
